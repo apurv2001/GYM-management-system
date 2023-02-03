ff35ada32244b32037563a768fb4d1bec9f77715
apurv2001 <82709369+apurv2001@users.noreply.github.com>
2023-02-03 23:44:10 +0530

    Add files via upload

--- about.php
+++ about.php
@@ -58,8 +58,7 @@ echo "<script>window.location.href='booking-history.php'</script>";
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-7 m-auto text-white">
-					<h2>Home</h2>
-					<p>Physical Activity  Can Improve Your Health</p>
+					<h2>About GYM Management System</h2>
 				</div>
 			</div>
 		</div>
@@ -72,49 +71,13 @@ echo "<script>window.location.href='booking-history.php'</script>";
 		<div class="container">
 			<div class="section-title text-center">
 				<img src="img/icons/logo-icon.png" alt="">
-				<h2>Pricing plans</h2>
-				<p>Practice Yoga for perfect physical beauty, take care of your soul and fully enjoy your life!</p>
+				<h2>About Us</h2>
 			</div>
 			<div class="row">
-				        <?php 
 
-$sql ="SELECT id, category, titlename, PackageType, PackageDuratiobn, Price, uploadphoto, Description, create_date from tbladdpackage";
-$query= $dbh -> prepare($sql);
-$query-> execute();
-$results = $query -> fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query -> rowCount() > 0)
-{
-foreach($results as $result)
-{
-?>
-				<div class="col-lg-3 col-sm-6">
-					<div class="pricing-item begginer">
-						<div class="pi-top">
-							<h4><?php echo $result->titlename;?></h4>
-						</div>
-						<div class="pi-price">
-							<h3><?php echo htmlentities($result->Price);?></h3>
-							<p>	<?php echo $result->PackageDuratiobn;?></p>
-						</div>
-						<ul>
-							<?php echo $result->Description;?>
-							
-						</ul>
-						<?php if(strlen($_SESSION['uid'])==0): ?>
-						<a href="login.php" class="site-btn sb-line-gradient">Booking Now</a>
-						<?php else :?>
-							<!-- <a href="#" class="site-btn sb-line-gradient">Booking Now</a> -->
-							 <form method='post'>
-                            <input type='hidden' name='pid' value='<?php echo htmlentities($result->id);?>'>
-                          
-
-                        <input class='site-btn sb-line-gradient' type='submit' name='submit' value='Booking Now' onclick="return confirm('Do you really want to book this package.');"> 
-                        </form> 
-							 <?php endif;?>
-					</div>
+				<div class="col-lg-12 col-sm-6">
+			<p>Hola amigos! We are a bunch of curious and intellectual professionals who out of boredom from the monotonous routine, established Brand Beavers in Mumbai to emerge as problem solvers. Our team of entrepreneurs, thinkers, strategists, designers, and technologists will help you to emerge as an effective and disruptive brand all over the globe. Our brainstorming and amicable individuals go beyond their comfort zone to satisfy the customer’s requirements.</p>
 				</div>
-				<?php  $cnt=$cnt+1; } } ?>
 			</div>
 		</div>
 	</section>
